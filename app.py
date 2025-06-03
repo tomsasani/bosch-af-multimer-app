1e0d6ede5d9449ca73b19c41ca5372659c92e5b3
Tom Sasani <tomsasani1493@gmail.com>
2025-06-03 10:51:50 -0400

    fix other path to xl

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ data_to_fields = (
 data_to_fields = {k.rstrip("\t"): v for k, v in data_to_fields.items()}
 
 data = pd.read_excel(
-    "InsR missense scores joined file JAB 5-16-25.xlsx",
+    "/home/tomsasani/bosch-af-multimer-app/InsR.xlsx",,
     sheet_name="InsR",
     index_col=[0, 1],
     header=[0, 1],
